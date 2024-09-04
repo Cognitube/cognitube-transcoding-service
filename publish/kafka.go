0e897cbbc26e3da01f641f86804b43a84b6ad98b
Haozhe Zhang <zhz19980514@gmail.com>
2024-09-04 17:36:09 -0500

    fix: remove sasl for local

--- publish/kafka.go
+++ publish/kafka.go
@@ -22,30 +22,35 @@ func (p *KafkaPublisher) PublishProd(topic string, message []byte) error {
 	eventHubNamespace := env.GetInstance().EventHubNamespace
 	connectionString := env.GetInstance().EventHubConnectionString
 	username := env.GetInstance().Username
-
 	bootstrapServers := env.GetInstance().KafkaBootstrapServers
 
 	var addr string
+	var transport kafka.Transport
+
 	if eventHubNamespace != "" {
 		addr = eventHubNamespace + ".servicebus.windows.net:9093"
+		// Set up SASL configuration for Event Hubs
+		mechanism := plain.Mechanism{
+			Username: username,
+			Password: connectionString,
+		}
+		transport = kafka.Transport{
+			SASL: mechanism,
+			TLS:  &tls.Config{}, // Ensure TLS is configured for Azure Event Hubs
+		}
 	} else {
 		addr = bootstrapServers
-	}
-
-	// Set up SASL configuration
-	mechanism := plain.Mechanism{
-		Username: username,
-		Password: connectionString,
+		// No SASL for local Kafka
+		transport = kafka.Transport{
+			TLS: nil, // No TLS for local Kafka (usually not needed)
+		}
 	}
 
 	writer := &kafka.Writer{
-		Addr:     kafka.TCP(addr),
-		Topic:    topic,
-		Balancer: &kafka.LeastBytes{},
-		Transport: &kafka.Transport{
-			SASL: mechanism,
-			TLS:  &tls.Config{},
-		},
+		Addr:      kafka.TCP(addr),
+		Topic:     topic,
+		Balancer:  &kafka.LeastBytes{},
+		Transport: &transport,
 	}
 	err := writer.WriteMessages(context.Background(),
 		kafka.Message{
